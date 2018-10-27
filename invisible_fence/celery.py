1555f2f460cbb8a15494ea424e7153f3e1402db4
Josh Sullivan <josh@magnolia.technology>
2018-10-27 14:02:47 -0400

    updated celery settings

--- invisible_fence/celery.py
+++ invisible_fence/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invisible_fence.settings')
@@ -15,6 +16,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
